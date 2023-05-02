59f207c705eb0baee9d41445acdabad84f2f42c6
Dan-Burns <dnlburns808@gmail.com>
2023-05-02 15:31:33 -0400

    fix typo

--- useful_functions.py
+++ useful_functions.py
@@ -147,13 +147,13 @@ def convert_rms_to_rgb(rmsf, color='red'):
 def parmed_underscore_topology(gromacs_processed_top, atom_indices, output_top):
     '''
     Add underscores to atom types of selected atoms.
-    This is usefule if using the plumed_scaled_topologies script 
+    This is useful if using the plumed_scaled_topologies script 
     for hremd system modification.
     With this, you still need to open the new topology file and delete the 
     underscores from the beginning of the file [atomtypes]
     or else plumed will look for atoms with 2 underscores to apply lambda to.
     '''
-    top = GromacsTopologyFile(gromacs_process_top)
+    top = GromacsTopologyFile(gromacs_processed_top)
 
     for atom in top.view[atom_indices].atoms:
         atom.type = f"{atom.type}_"
