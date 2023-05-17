a8a5fbbbaf75cfda57e9f9c869cbf4ff30c46f5a
Dan-Burns <dnlburns808@gmail.com>
2023-05-17 14:52:28 -0500

    debug get_rmsd and add frames option

--- basic_analyses.py
+++ basic_analyses.py
@@ -1,29 +1,36 @@
 
 from MDAnalysis.analysis.rms import rmsd
 
-def get_rmsd(universe, stride=1, selections=None):
+def get_rmsd(universe, stride=1, selections=None, frames=None):
 
-    # Should just use mda 2's RMSD.run() function
+    # instead of using mda 2's RMSD.run() function, this lets you select a subset of frames
 
     u = universe
 
     if selections is None:
-
-    selections['protein'] = u.select_atoms('protein')
- 
-
+    
+        selections = {'protein': u.select_atoms('protein')}
+    
     u.trajectory[0]   # rewind trajectory
     xref0 = dict((name, g.positions - g.center_of_mass()) for name, g in selections.items())
 
     nframes = len(u.trajectory[::stride])
     results = dict((name, np.zeros((nframes, 2), dtype=np.float64)) for name in selections)
-
-    for iframe, ts in enumerate(u.trajectory[::stride]):
-        for name, g in selections.items():
-            results[name][iframe, :] = (u.trajectory.time,
-                                        rmsd(g.positions, xref0[name],
-                                            center=True, superposition=True))
+    
+    if frames is not None:
+        for iframe, ts in enumerate(u.trajectory[frames]):
+            for name, g in selections.items():
+                results[name][iframe, :] = (u.trajectory.time,
+                                            rmsd(g.positions, xref0[name],
+                                                center=True, superposition=True))
+    else:
+        for iframe, ts in enumerate(u.trajectory[::stride]):
+            for name, g in selections.items():
+                results[name][iframe, :] = (u.trajectory.time,
+                                            rmsd(g.positions, xref0[name],
+                                                center=True, superposition=True))
+        
    
-   return results
+    return results
         
    
